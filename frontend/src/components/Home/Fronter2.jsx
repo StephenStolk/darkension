a2c4c864231ece4a0cec0cb83d72634d337db3dc
Stephen Stolk <shikharburman2@gmail.com>
2024-01-31 23:22:13 +0530

    Updated image locations in pages

--- frontend/src/components/Home/Fronter2.jsx
+++ frontend/src/components/Home/Fronter2.jsx
@@ -13,19 +13,19 @@ const Fronter2 = () => {
             <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start md:gap-10 gap-8'>
             <div className='bg-[rgba(255,255,255,0.04)] rounded-[35px] h-96 shadow-3xl p-8 items-center justify-center hover:-translate-y-4 transition-all duration-300'>
                 <div className='w-[full] h-[full] justify-center mx-auto'>
-                <img src="./public/f1.png" className='w-[80%] mx-auto'></img>
+                <img src="f1.png" className='w-[80%] mx-auto'></img>
                 <p className='mx-[1rem] text-white text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
                 </div>
             </div>
             <div className='bg-[rgba(255,255,255,0.04)] rounded-[35px] h-96 shadow-3xl p-8 items-center justify-center hover:-translate-y-4 transition-all duration-300 md:mt-16'>
                 <div className=''>
-                <img src="./public/f1.png" className='w-[80%]'></img>
+                <img src="f1.png" className='w-[80%]'></img>
                 <p className='mx-[1rem] text-white text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
                 </div>
             </div>
             <div className='bg-[rgba(255,255,255,0.04)] rounded-[35px] h-96 shadow-3xl p-8 items-center justify-center hover:-translate-y-4 transition-all duration-300'>
                 <div className=''>
-                <img src="./public/f1.png" className='w-[80%]'></img>
+                <img src="f1.png" className='w-[80%]'></img>
                 <p className='mx-[1rem] text-white text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
                 </div>
             </div>
