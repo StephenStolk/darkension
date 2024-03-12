19721e81ace72800b6bc939405296a2653b920e2
Shikhar_Burman <134587780+StephenStolk@users.noreply.github.com>
2024-03-12 23:59:41 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eokayslint-disable no-unused-vars */
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Publication from './pages/Publication'
